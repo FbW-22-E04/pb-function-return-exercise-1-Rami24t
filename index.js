38be4588ee7179b17f947700bbbd4cef48e0cdd1
Rami <103028944+Rami24t@users.noreply.github.com>
2022-07-18 15:25:56 +0200

    solution

--- index.js
+++ index.js
@@ -2,26 +2,37 @@
 // 1
 function uppercase(str) {
   //write your code here 
-  words = str.split(' ');
-  words.forEach(element => {
-    element = element[0].toUpperCase()+element.slice(1);
-  });
-  return words.join(' ');
+  let words = str.split(' ');
+  words.forEach(element => words[words.indexOf(element)] = (element[0].toUpperCase() + element.slice(1)));
+  // console.log('7: ', words);
+  words = words.join(' ');
+  return words;
 }
-
 console.log(uppercase("the quick brown fox"));
 
 // 2
 // Write a JavaScript function that accepts a string as a parameter and find the longest word within the string.
 function find_longest_word(str) {
   //write your code here 
+  words = str.split(' ');
+  longest = '';
+  words.forEach(element => {
+      if (element.length > longest.length)
+          longest = element;
+  });
+  return longest;
 }
 console.log(find_longest_word("Web Development Tutorial"));
 
 // 3
 // Write a JavaScript function that accept a list of country names as input and returns the longest country name as output.
-function Longest_Country_Name(country_name) {
-  //write your code here 
+function Longest_Country_Name(words) {
+  longest = '';
+  words.forEach(element => {
+      if (element.length > longest.length)
+          longest = element;
+  });
+  return longest;
 }
 
 console.log(
